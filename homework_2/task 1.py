383af62b373c448f378a5e534f5dbf1a189c40e9
nino <nino.mtchedlidze@iset.ge>
2024-02-29 18:42:27 +0400

    Homework 2

---
+++ homework_2/task 1.py
@@ -0,0 +1,8 @@
+User_input=input('please enter true or false: ')
+if User_input=="true":
+    print('whoala')
+elif User_input=='false':
+    print('input is false')
+
+else:
+    print('please reenter')
