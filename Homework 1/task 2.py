72f11b394f9671e857cd612c167a9c36be2002b8
nino <nino.mtchedlidze@iset.ge>
2024-02-29 16:43:45 +0400

    homework 1

--- Homework 1/task 2.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-current_year = datetime.datetime.now().year
-print(current_year)
-
-Name=input('შეიყვანეთ სახელი: ')
-Birth_Year=int(input('შეიყვანეთ დაბადების წელი:'))
-print('გამარჯობა,',Name,'თქვენ ხართ',Birth_Year-current_year,'წლის')
