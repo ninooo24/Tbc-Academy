70be65e03e708d535434d5fceac24c2f9774f2ff
nino <nino.mtchedlidze@iset.ge>
2024-02-29 16:06:37 +0400

    homework2

---
+++ Homework 2/task 1.py
@@ -0,0 +1,10 @@
+# 1. Დაწერეთ პროგრამა რომელიც მიიღებს true ან false. Თუ პროგრამამ მიიღო true
+# , ეკრანზე დაბეჭდეთ “whoala”.
+User_input=input('please enter true or false: ')
+if User_input=="true":
+    print('whoala')
+elif User_input=='false':
+    print('good')
+
+else:
+    print('please reenter')
