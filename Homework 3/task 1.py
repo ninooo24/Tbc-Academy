84d42777e8519600fe9376b1916831fb67ad144b
nino <nino.mtchedlidze@iset.ge>
2024-03-06 15:16:03 +0400

    homework3

--- Homework 3/task 1.py
+++ Homework 3/task 1.py
@@ -1,7 +1,7 @@
 import math
 
-x=float(input('please enter the value of x: '))
-y=float(input('please enter the value of y: '))
-t=pow(x,y)
-print('x in power y is',t)
+x = float(input('please enter the value of x: '))
+y = float(input('please enter the value of y: '))
+z = pow(x,y)
 
+print('x in power y is', z)
--- Homework 3/task 2.py
+++ Homework 3/task 2.py
@@ -4,20 +4,16 @@ birth_year = int(input("Enter the year of birth: "))
 birth_month = int(input("Enter the month of birth(1-12): "))
 birth_day = int(input("Enter the day of birth(1-31): "))
 
-
-if birth_year<0 or birth_year>2024:
-    print('pleas enter the valid year')
-elif birth_month>12 or birth_month<0:
+if birth_year < 1 or birth_year > 2024:
+    print('please enter the valid year')
+elif birth_month > 12 or birth_month < 1:
     print('please enter the valid month ')
-elif birth_day>31 or birth_day<0:
+elif birth_day > 31 or birth_day < 1:
     print('please enter the valid day')
+elif birth_month == 2 and birth_day > 28:
+    print('please enter the correct day(in February we have 28-29 days)')
 else:
     birth_date = datetime.date(birth_year, birth_month, birth_day)
     week_day = birth_date.strftime("%A")
     print("The weekday of the birth date is:", week_day)
-
-
-
-
-
 
