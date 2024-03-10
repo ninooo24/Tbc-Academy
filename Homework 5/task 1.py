098f17f6adeb8e5892e08e735b599c6f3774b91a
Nino_Mtchedlidze <161021104+ninooo24@users.noreply.github.com>
2024-03-10 21:49:54 +0400

    Update task 1.py

--- Homework 5/task 1.py
+++ Homework 5/task 1.py
@@ -6,12 +6,9 @@ else:
     for i in range(1, n):
         print(i, "-", end="")
         count = 0
-        for j in range(1, i):
+        for j in range(1, i + 1):
             if i % j == 0:
                 count = count + 1
                 if count <= 3:
                     print(j, end=" ")
         print()
-
-
-
