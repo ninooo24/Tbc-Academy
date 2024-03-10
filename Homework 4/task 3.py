9bc50adab7df420de043dbaa067b4e181e036c18
nino <nino.mtchedlidze@iset.ge>
2024-03-10 21:48:48 +0400

    homework4

--- Homework 4/task 3.py
+++
@@ -1,8 +0,0 @@
-number = int(input("Enter number between 0-1000: "))
-if number <= 0 or number > 1000:
-    print('please enter number between 0-1000')
-else:
-    for i in range(1, number + 1):
-        if number % i == 0:
-            print(i, end=" ")
-
