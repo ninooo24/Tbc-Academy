8a141250395845e9beaf1a98fcacab83972b01dd
nino <nino.mtchedlidze@iset.ge>
2024-03-16 13:15:41 +0400

    homework7

---
+++ Homework 7/task 2.py
@@ -0,0 +1,14 @@
+n = int(input("Please enter n between 0-1000: "))
+while n <= 0 or n > 1000:
+    n = int(input("Please enter the correct number: "))
+
+print(n, end="")
+
+while n != 1:
+    if n % 2 == 0:
+        n = n // 2
+    else:
+        n = 3 * n + 1
+    print("->", n,end="")
+
+
